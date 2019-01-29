5b3f38f6bdb158aad7bba8834d96526cf1dd3a54
sleungdtt <sleung@deloitte.com.hk>
2019-01-29 17:23:30 +0800

    Android working

---
+++ FCMDemoApp/FCMDemoApp.Android/MainActivity.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Iid;
+using Firebase.Messaging;
+
+namespace FCMDemoApp.Droid
+{
+    [Activity(Label = "FCMDemoApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static readonly string TAG = "MainActivity";
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            IsPlayServicesAvailable();
+
+            CreateNotificationChannel();
+
+            FirebaseMessaging.Instance.SubscribeToTopic("news");
+            Log.Debug(TAG, "Subscribed to remote notifications");
+            Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+            LoadApplication(new App());
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Error(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Error(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                Log.Info(TAG, "Google Play Services is available.");
+                return true;
+            }
+        }
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+    }
+}
